f3cf38bca3547874ad89c134232fd09761bb602a
Clifford <ts0260140@gmail.com>
2021-01-11 09:31:38 +0800

    feat(app.js): searchField

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
     super();
     this.state = {
       monsters: [],
+      searchField: '',
     };
   }
   componentDidMount() {
@@ -17,6 +18,16 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={e =>
+            // setState 是一個非同步的事件,可以利用setState的第二個參數 再往下做
+            this.setState({ searchField: e.target.value }, () =>
+              console.log(this.state)
+            )
+          }
+        />
         <CardList monsters={this.state.monsters} />
       </div>
     );
